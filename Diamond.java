39c46a8d977ba2bba18f4ae78b0edc704c42ab02
DontHackMeMan <cjfludx@gmail.com>
2024-10-14 22:46:34 -0400

    Update Diamond.java
    Odd Section Completed

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,59 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int Diamond;
+    int space = 1;
+
+
+
+
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number");
+    Diamond = scan.nextInt();
+    int mod = Diamond % 2;
+    if (mod ==  1){
+      while(space < Diamond){
+        System.out.println(" ".repeat(Diamond-(space/2))+"*".repeat(space));
+        space++;
+        space++;
+      }
+
+
+      while(space > 0){
+        System.out.println(" ".repeat(Diamond-(space/2))+"*".repeat(space));
+        space--;
+        space--;
+
+      }
+    }
+    if (mod == 0){
+      space = 0;
+      while(space < Diamond){
+
+        space++;
+        space++;
+        System.out.println(" ".repeat(Diamond-(space/2))+" *".repeat(space));
+        space++;
+        space++;
+      }
+
+
+      while(space > 0){
+
+        System.out.println(" ".repeat(Diamond-(space/2))+" *".repeat(space));
+        space--;
+        space--;
+
+      }
+    }
+    scan.close();
   }
 }
+
+
+
+
+
+
+
